e656b02ebe280b3b4dc2ecd3b5c4f79785160b0d
Elise Ngo <elisetvy@gmail.com>
2024-11-08 14:03:43 -0800

    Edit width for responsiveness

--- src/Brief/Brief.tsx
+++ src/Brief/Brief.tsx
@@ -4,7 +4,7 @@ function Brief() {
   return (
     <main className="Brief">
       <header>Brief</header>
-      <p>Create a page to enhance team experience</p>
+      <p>Create a platform to enhance player experience with streamlined access to stats, schedules, and updates, while strengthening brand identity with a professional image that boosts visibility, simplifies team info, and encourages inquiries from prospective players.</p>
     </main>
   );
 }
