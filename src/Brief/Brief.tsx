022552de162aa8abe0efda2ec5b0266b0be20ac7
Elise Ngo <elisetvy@gmail.com>
2024-11-08 13:06:23 -0800

    Create Brief component

---
+++ src/Brief/Brief.tsx
@@ -0,0 +1,12 @@
+import "./Brief.css";
+
+function Brief() {
+  return (
+    <main className="Brief">
+      <header>Brief</header>
+      <p>Create a page to enhance team experience</p>
+    </main>
+  );
+}
+
+export default Brief;
